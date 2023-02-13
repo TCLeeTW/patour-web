7dcd5b52709612d3b3c686a3ddaa139ed15dade4
RBaronT <t.c.lee.taiwan@gmail.com>
2023-02-13 17:10:47 +0800

    Restructure folders

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import '../styles/App.css';
+import Header from "./Header";
+import Footer from "./Footer"
+import Body from "./Body"
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Body/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
