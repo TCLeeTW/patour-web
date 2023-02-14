f6f909129c8d6121111576966e811c20f1bdae12
RBaronT <t.c.lee.taiwan@gmail.com>
2023-02-14 12:11:12 +0800

    Apply Header Section and Hero Section template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,20 @@
-import '../styles/App.css';
+import React from "react";
 import Header from "./Header";
-import Footer from "./Footer"
-import Body from "./Body"
+import Footer from "./Footer";
+import Body from "./Body";
+import HeaderSection from "./HeaderSection";
+import HeroSection from "./HeroSection"
+import '../styles/App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      {/* <Header/>
       <Body/>
-      <Footer/>
+      <Footer/> */}
+      <HeaderSection/>
+      <HeroSection/>
+      
     </div>
   );
 }
